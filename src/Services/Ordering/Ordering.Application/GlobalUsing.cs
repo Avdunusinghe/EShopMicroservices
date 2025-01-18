3071defb8ce3d53487f678677d21653b62ea33bc
Avdunusinghe <avdunusinghe@gmail.com>
2025-01-18 17:26:46 +0530

    🔧 chore(Ordering Service): Implemented Clean Architecture and DDD
    - 🏗️ Structured the Ordering Service based on Clean Architecture principles.
    - 📦 Adopted Domain-Driven Design (DDD) for the domain layer implementation.
    - 🧩 Defined aggregates, entities, and value objects to encapsulate business logic effectively.
    
    #Chore #CleanArchitecture #DDD

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,6 +1,8 @@
-﻿namespace Ordering.Application
-{
-    internal class GlobalUsing
-    {
-    }
-}
+﻿global using BuildingBlocks.CQRS;
+global using Microsoft.EntityFrameworkCore;
+global using Ordering.Application.Data;
+global using Ordering.Application.Dtos;
+global using Ordering.Application.Exceptions;
+global using Ordering.Application.Extensions;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
