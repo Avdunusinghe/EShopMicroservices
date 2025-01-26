f89ee739576e8a0331266b15512a0f466523555c
Avdunusinghe <avdunusinghe@gmail.com>
2025-01-26 13:07:40 +0530

    🐳 chore: Containerize and Orchestrate Shopping.Web Application in Docker-Compose
    🚀 Configured Docker and Docker-Compose to containerize the Shopping.Web application.
    - Added Dockerfile for the Shopping.Web application.
    - Created a docker-compose.yml file for service orchestration.
    - Ensured seamless integration with other microservices in the architecture.
    
    #Chore #Docker #DockerCompose #Microservices #ShoppingWebApp

--- src/WebApps/Shopping.Web/GlobalUsing.cs
+++ src/WebApps/Shopping.Web/GlobalUsing.cs
@@ -4,3 +4,4 @@ global using Refit;
 global using Shopping.Web.Models.Basket;
 global using Shopping.Web.Models.Catalog;
 global using Shopping.Web.Models.Ordering;
+global using Shopping.Web.Services;
